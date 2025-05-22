9c4efec0640b55164feef58cfd568907b8c10135
aman3255 <amanprajapati3255@gmail.com>
2025-05-23 02:27:54 +0530

    Fix time selection display in event form and set custom application favicon

--- src/components/CalendarHeader.tsx
+++ src/components/CalendarHeader.tsx
@@ -125,7 +125,7 @@ const CalendarHeader: React.FC = () => {
           </div>
         </div>
 
-        <div className="flex justify-end">
+        {/* <div className="flex justify-end">
           <div className="relative w-full md:w-64">
             <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
               <Search className="h-5 w-5 text-gray-400" />
@@ -138,7 +138,7 @@ const CalendarHeader: React.FC = () => {
               className="pl-10 pr-4 py-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
             />
           </div>
-        </div>
+        </div> */}
       </div>
     </header>
   );
